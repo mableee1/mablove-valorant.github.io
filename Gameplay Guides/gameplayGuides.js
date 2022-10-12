6d98ca2f95a76647de1e6f68c650f1e00b1bcd6f
Natalia Kim <46171209+mableee1@users.noreply.github.com>
2022-10-12 13:39:39 +0300

    :wq

--- Gameplay Guides/gameplayGuides.js
+++ Gameplay Guides/gameplayGuides.js
@@ -53,60 +53,87 @@ ggSearch.addEventListener("mouseover", () => {
 ggSearch.addEventListener("mouseout", () => {
     mouse("mouseout", ggSearch, "searchButtonHover");
 });
+
+
+
 // SECTION
 // new guides subscription form (e-mail)
 
 // add new guide (only for signed up users)
 
 // filters
-// filter by agent
-// .elementVisibility
-// при выборе агента, убрать остальные .agentListBlock
-// переделать в grid, так как grid является многомерным, а flexbox - двумерный
 let agentFilter = document.querySelector("#agentSelect");
+let roleFilter = document.querySelector("#roleSelect");
+let invisibilityClass = "elementInvisibility"; // agent card is invisible
+let agentLiClass = "agentLi"; // agent card is visible
 let agentCards = document.querySelectorAll(".agentLi");
+// FUNCTIONS FOR DIFFERENT FILTERS' VALUES
+function allChosen(card) { // all agents, all roles
+    if (card.classList.contains(invisibilityClass)) {
+        card.classList.remove(invisibilityClass);
+        card.classList.add(agentLiClass);
+    }
+}
+function roleChosen(card) { // all agents, role chosen
+    if (!card.classList.contains(chosenRole) && card.classList.contains(agentLiClass)) {
+        card.classList.remove(agentLiClass);
+        card.classList.add(invisibilityClass);
+    }
+    if (card.classList.contains(chosenRole) && card.classList.contains(invisibilityClass)) {
+        card.classList.remove(invisibilityClass);
+        card.classList.add(agentLiClass);
+    }
+}
+function agentChosen() { // all roles, agent chosen
+    if (!item.classList.contains(chosenAgent) && item.classList.contains(agentLiClass)) {
+        item.classList.remove(agentLiClass);
+        item.classList.add(invisibilityClass);
+    } else if (item.classList.contains(chosenAgent) && item.classList.contains(invisibilityClass)) {
+        item.classList.remove(invisibilityClass);
+        item.classList.add(agentLiClass);
+    }
+}
+function bothChosen(card) { // agent chosen, role chosen
+    if (card.classList.contains(chosenAgent) && card.classList.contains(chosenRole) && card.classList.contains(invisibilityClass)) {
+        card.classList.remove(invisibilityClass);
+        card.classList.add(agentLiClass);
+    }
+    if ((!card.classList.contains(chosenAgent) || !card.classList.contains(chosenRole)) && card.classList.contains(agentLiClass)) {
+        card.classList.remove(agentLiClass);
+        card.classList.add(invisibilityClass);
+    }
+}
+// FILTER BY AGENT
 agentFilter.addEventListener("change", () => {
-    if (agentFilter.value !== "allAgents") {
-        let chosenAgent = agentFilter.value;
-        agentCards.forEach((item) => {
-            if (!item.classList.contains(chosenAgent) && item.classList.contains("agentLi")) {
-                item.classList.remove("agentLi");
-                item.classList.add("elementInvisibility");
-            } else if (item.classList.contains(chosenAgent) && !item.classList.contains("agentLi") && item.classList.contains("elementInvisibility")) {
-                item.classList.remove("elementInvisibility");
-                item.classList.add("agentLi");
-            }
-        });
-    } else if (agentFilter.value === "allAgents") {
-        agentCards.forEach((item) => {
-            if (!item.classList.contains("agentLi") && item.classList.contains("elementInvisibility")) {
-                item.classList.remove("elementInvisibility");
-                item.classList.add("agentLi");
-            }
-        });
+    let chosenAgent = agentFilter.value;
+    let chosenRole = roleFilter.value;
+    if (chosenAgent === "allAgents" && chosenRole === "allRoles") {
+        agentCards.forEach(allChosen(item));
+    }
+    if (chosenAgent === "allAgents" && chosenRole !== "allRoles") {
+        agentCards.forEach(roleChosen(item));
+    }
+    if (chosenAgent !== "allAgents" && chosenRole === "allRoles") {
+        agentCards.forEach(agentChosen());
+    } else if (chosenAgent !== "allAgents" && chosenRole !== "allRoles") {
+        agentCards.forEach(bothChosen(item));
     }
 });
-// filter by role
-let roleFilter = document.querySelector("#roleSelect");
+// FILTER BY ROLE
 roleFilter.addEventListener("change", () => {
     let chosenRole = roleFilter.value;
-    if (chosenRole !== "allRoles") {
-        agentCards.forEach((item) => {
-            if (!item.classList.contains(chosenRole)) {
-                item.classList.remove("agentLi");
-                item.classList.add("elementInvisibility");
-            } else if (item.classList.contains(chosenRole) && item.classList.contains("elementInvisibility")) {
-                item.classList.remove("elementInvisibility");
-                item.classList.add("agentLi");
-            }
-        });
-    } else {
-        agentCards.forEach((item) => {
-            if (item.classList.contains("elementInvisibility")) {
-                item.classList.remove("elementInvisibility");
-                item.classList.add("agentLi");
-            }
-        });
+    let chosenAgent = agentFilter.value;
+    if (chosenRole !== "allRoles" && chosenAgent !== "allAgents") {
+        agentCards.forEach(bothChosen(item));
+    }
+    if (chosenRole !== "allRoles" && chosenAgent === "allAgents") {
+        agentCards.forEach(roleChosen(item));
+    }
+    if (chosenRole === "allRoles" && chosenAgent !== "allAgents") {
+        agentCards.forEach(agentChosen(item));
+    }
+    if (chosenRole === "allRoles" && chosenAgent === "allAgents") {
+        agentCards.forEach(allChosen(item));
     }
 });
 // FOOTER
