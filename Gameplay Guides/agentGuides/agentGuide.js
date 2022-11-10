ea1d4b868e35d8bc8b656198ab31633c64140413
Natalia Kim <natasha_kim5@yahoo.com>
2022-11-10 03:00:29 +0300

    idk

--- Gameplay Guides/agentGuides/agentGuide.js
+++ Gameplay Guides/agentGuides/agentGuide.js
@@ -2,12 +2,11 @@ let navbar = document.querySelector(".navContainer");
 // document.documentElement (html)
 // document.head (head)
 // document.body (body)
-let pageBackground = document.querySelector(".pageBackground");
 // window.pageYOffset (scrolled distance from top)
 // window.pageXOffset (scrolled distance from left)
 // element.offsetHeight (total height of an element)
 window.addEventListener("scroll", () => {
-    if (window.pageYOffset > pageBackground.offsetHeight) {
+    if (window.pageYOffset > navbar.offsetHeight) {
         navbar.classList.add("scrolledNav");
         console.log(window.pageYOffset);
     } else if (navbar.classList.contains("scrolledNav") && window.pageYOffset === 0) {
--- Gameplay Guides/gameplayGuides.js
+++ Gameplay Guides/gameplayGuides.js
@@ -1,50 +1,14 @@
 // HEADER
-// show searchBar when clicking on the search button
-let searchBar = document.querySelector(".searchBar");
-let searchButton = document.querySelector(".searchButton");
-searchButton.addEventListener("click", () => {
-    searchBar.classList.toggle("elementVisibility");
-});
-// change the color of an option when hovering on it
-
-
-// change the color of the SIGN UP button when hovering on it
-let signupButton = document.querySelector(".signupButton");
-let signupLink = document.querySelector(".signupLink");
-signupButton.addEventListener("mouseover", () => {
-    signupButton.classList.add("signupButtonHover");
-    signupLink.classList.add("menuOptionHover");
-});
-signupButton.addEventListener("mouseout", () => {
-    signupButton.classList.remove("signupButtonHover");
-    signupLink.classList.remove("menuOptionHover");
-});
-
-// change the shadow of the search button when hovering on it
-function mouse(ev, targetElement, cl) {
-    switch(ev) {
-        case "mouseover":
-            targetElement.classList.add(cl);
-            break;
-        case "mouseout":
-            targetElement.classList.remove(cl);
-            break;
-        case "click":
-            targetElement.classList.toggle(cl);
-            break;
+// show the navbar on scroll
+window.addEventListener("scroll", () => {
+    let navContainer = document.querySelector(".navContainer");
+    if (window.pageYOffset > navContainer.offsetHeight) {
+        navContainer.classList.add("navScrolled");
     }
-    console.log("Changes to the search button has been applied.");
-}
-let ggSearch = document.querySelector(".gg-search");
-ggSearch.addEventListener("mouseover", () => {
-    mouse("mouseover", ggSearch, "searchButtonHover");
-    
+    else if (window.pageYOffset <= navContainer.offsetHeight && navContainer.classList.contains("navScrolled")) {
+        navContainer.classList.remove("navScrolled");
+    }
 });
-ggSearch.addEventListener("mouseout", () => {
-    mouse("mouseout", ggSearch, "searchButtonHover");
-});
-
-
 
 // SECTION
 // new guides subscription form (e-mail)
